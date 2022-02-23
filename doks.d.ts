9514dc0771665ca03d3b8a3d585e0da7c5444d43
Michael Klein <m.klein@talkwalker.com>
2022-02-23 15:22:27 +0100

    fix types

--- doks.d.ts
+++ doks.d.ts
@@ -3,8 +3,7 @@ import * as main from "./src/main";
 import * as components from "./src/components/components";
 declare module "doks" {
   export = interfaces;
-  export type docs = typeof main.docs;
-  export type ComponentContextProvider =
-    typeof components.ComponentContextProvider;
+  export const docs: typeof main.docs;
+  export const ComponentContextProvider: typeof components.ComponentContextProvider;
   export type ComponentsContext = components.ComponentsContext;
 }
